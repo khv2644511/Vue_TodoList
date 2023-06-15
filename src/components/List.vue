81b660cf9226d9725bc6224595efa81a1421f65f
hyebin9808 <khv2644511@naver.com>
2023-06-15 16:38:20 +0900

    feat: 버튼 추가 및 스타일링

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,9 +1,18 @@
 <template>
-  <div class="orange pa-2">
+  <div class="pa-2">
     <h2>투두 리스트</h2>
     <v-card class="pa-1 ma-1" v-for="(list, index) in todoList" :key="index">
       <p class="ma-0">{{ list.memo }}</p>
     </v-card>
+    <v-btn icon fab small color="green"
+      ><v-icon color="green">mdi-check</v-icon>
+    </v-btn>
+    <v-btn icon fab small color="blue"
+      ><v-icon color="blue">mdi-refresh</v-icon></v-btn
+    >
+    <v-btn icon fab small color="red"
+      ><v-icon color="red">mdi-delete</v-icon></v-btn
+    >
   </div>
 </template>
 
