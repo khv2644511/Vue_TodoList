cc264f28710f4987458a39393f01e9446375320f
hyebin9808 <khv2644511@naver.com>
2023-06-15 14:04:39 +0900

    init;

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,9 +10,15 @@ module.exports = {
   ],
   parserOptions: {
     parser: "@babel/eslint-parser",
+    requireConfigFile: false,
   },
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    // "vue/multi-word-component-names": false,
+    "lintOnSave": false,
+    "vue/multi-word-component-names": ["error", {
+      "ignores": ['default']
+  }]
   },
 };
---
+++ src/views/Home.vue
@@ -0,0 +1,12 @@
+<template><div-container></div-container></template>
+
+<script>
+import List from '@/components/List.vue'
+import ListAdd from '@/components/ListAdd.vue'
+
+export default {
+  name: "Home",
+
+  components: {List, ListAdd},
+};
+</script>
