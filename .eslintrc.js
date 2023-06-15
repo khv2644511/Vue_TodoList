369b79f7c59d2d3e68ff2ea6931b0af23d04e03e
hyebin9808 <khv2644511@naver.com>
2023-06-15 15:56:06 +0900

    lint: eslint 변경 및 auto fix on save 모드

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,22 +4,38 @@ module.exports = {
     node: true,
   },
   extends: [
-    "plugin:vue/essential",
-    "eslint:recommended",
-    "plugin:prettier/recommended",
+    'plugin:vue/essential',
+    'eslint:recommended',
+    'plugin:prettier/recommended',
   ],
+  plugin: [
+    'vue'
+  ],
+  parser: "vue-eslint-parser",
   parserOptions: {
-    parser: "@babel/eslint-parser",
+    parser: '@babel/eslint-parser',
     requireConfigFile: false,
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "lintOnSave": false,
-    "vue/multi-word-component-names": ["error", {
-      "ignores": ['default']
-    }],
-    "prettier/prettier": ["error", { "endOfLine": "auto" }]   
-
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    lintOnSave: false,
+    'vue/multi-word-component-names': [
+      'error',
+      {
+        ignores: ['default'],
+      },
+    ],
+    'prettier/prettier': ['error', {
+      singleQuote: true,
+      semi: true,
+      useTabs: true,
+      tabWidth: 2,
+      trailingComma: 'all',
+      printWidth: 80,
+      bracketSpacing: true,
+      arrowParens: 'avoid',
+      endOfLine: 'auto',
+    },],
   },
 };
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,13 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: ["vuetify"],
-  lintOnSave:false
+  lintOnSave: false,
+  chainWebpack: config => {
+    config
+      .plugin('eslint')
+      .tap(args => {
+        args[0].fix = true
+        return args
+      })
+  }
 });
