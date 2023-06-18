8cc5516764d35856a65838cb3857b4236a26773d
hyebin9808 <khv2644511@naver.com>
2023-06-18 13:29:17 +0900

    style: 투두 완료 시 스타일 변화주기

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="pa-2">
     <h2>투두 리스트</h2>
-    <v-card class="pa-1 ma-1" v-for="(list, index) in todoList" :key="index">
+    <v-card
+      class="pa-1 ma-1"
+      :class="{ done: list.status === 'done' }"
+      v-for="(list, index) in todoList"
+      :key="index"
+    >
       <p class="ma-0">{{ list.memo }}</p>
       <p class="ma-0">{{ list.status }}</p>
       <v-btn
@@ -40,3 +45,13 @@
     props: ["todoList"],
   };
 </script>
+
+<style scoped>
+  .done {
+    background-color: #b7b7b6;
+  }
+  .done p {
+    text-decoration: line-through;
+    color: rgba(0, 0, 0, 0.5);
+  }
+</style>
