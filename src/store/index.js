c9d71a4e553b8e1aa2f2446a13e35d95b1874845
hyebin9808 <khv2644511@naver.com>
2023-06-20 13:19:08 +0900

    refactor

--- src/store/index.js
+++ src/store/index.js
@@ -26,23 +26,16 @@ export default new Vuex.Store({
     },
   },
   actions: {
-    // 로그인 시도해서 로그인 성공 시 loginSuccess 커밋
     login({ state, commit }, loginObj) {
       console.log(loginObj);
-      // 전체 유저에서 해당 이메일로 유저를 찾는다.
       let selectedUser = null;
       state.allUsers.forEach((user) => {
         if (user.email === loginObj.email) selectedUser = user;
       });
-      if (selectedUser === null) commit("loginError");
-      else {
-        selectedUser.password !== loginObj.password
-          ? commit("loginError")
-          : commit("loginSuccess");
-      }
-      // 그 유저의 비밀번호와 입력된 비밀번호를 비교한다.
+      selectedUser === null || selectedUser.password !== loginObj.password
+        ? commit("loginError")
+        : commit("loginSuccess");
     },
-    // 로그인 실패
   },
   modules: {},
 });
