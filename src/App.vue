c6e8896d90796308c63e41dfd2937b8c9b761b0b
hyebin9808 <khv2644511@naver.com>
2023-06-20 11:23:01 +0900

    style: 전역 스타일링

--- src/App.vue
+++ src/App.vue
@@ -1,62 +1,55 @@
 <template>
   <v-app>
-    <v-container class="pa-0"
-  >
-      <v-app-bar
-        color="deep-purple"
-        dark
-      >
+    <v-container class="pa-0">
+      <v-app-bar color="deep-purple" dark>
         <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
-  
+
         <v-toolbar-title>Todo</v-toolbar-title>
       </v-app-bar>
 
-    <v-navigation-drawer
-      v-model="drawer"
-      absolute
-      temporary
-    >
-      <v-list
-        nav
-        dense
-      >
-        <v-list-item-group
-          v-model="group"
-          active-class="deep-purple--text text--accent-4"
-        >
-          <v-list-item router :to="{name: 'home'}" exact>
-            <v-list-item-icon>
-              <v-icon>mdi-home</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>Home</v-list-item-title>
-          </v-list-item>
+      <v-navigation-drawer v-model="drawer" absolute temporary>
+        <v-list nav dense>
+          <v-list-item-group
+            v-model="group"
+            active-class="deep-purple--text text--accent-4"
+          >
+            <v-list-item router :to="{ name: 'home' }" exact>
+              <v-list-item-icon>
+                <v-icon>mdi-home</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title>Home</v-list-item-title>
+            </v-list-item>
 
-          <v-list-item router :to="{name: 'login'}" exact>
-            <v-list-item-icon>
-              <v-icon>mdi-account</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>Login</v-list-item-title>
-          </v-list-item>
-        </v-list-item-group>
-      </v-list>
-    </v-navigation-drawer>
-    
-    <v-main fill-height>
-      <router-view></router-view>
-    </v-main>
-  </v-container>
+            <v-list-item router :to="{ name: 'login' }" exact>
+              <v-list-item-icon>
+                <v-icon>mdi-account</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title>Login</v-list-item-title>
+            </v-list-item>
+          </v-list-item-group>
+        </v-list>
+      </v-navigation-drawer>
+
+      <v-main>
+        <router-view></router-view>
+      </v-main>
+    </v-container>
   </v-app>
 </template>
 
 <script>
-import Home from "./views/Home.vue";
+  import Home from "./views/Home.vue";
 
-export default {
-  name: "App",
-  data: () => ({
-    drawer: false,
+  export default {
+    name: "App",
+    data: () => ({
+      drawer: false,
       group: null,
-  }),
-  components: { Home },
-};
+    }),
+    components: { Home },
+  };
 </script>
+
+<style lang="scss">
+  @import "@/assets/scss/common.scss";
+</style>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,65 +1,71 @@
 <template>
-   <v-container fill-height style="max-width:400px;">
-    <v-row no-gutters >
-      <v-col align-self="center">
-        <v-text-field
-            label="Email address"
-            v-model="email"></v-text-field>
-        <v-text-field
-          label="password"
-          type="password"
-          v-model="password"></v-text-field>
-        <v-btn large block depressed color="primary" @click='login'>로그인</v-btn>
+  <v-container style="max-width: 400px">
+    <v-row align-content="center" style="height: 100vh">
+      <v-alert width="400px" type="error" :value="isError" dismissible
+        >아이디와 비밀번호를 확인해주세요.</v-alert
+      >
+      <v-alert type="success" :value="loginSuccess" dismissible
+        >로그인이 완료되었습니다.</v-alert
+      >
+      <v-col>
+        <v-card>
+          <v-toolbar flat color="primary">
+            <v-toolbar-title style="color: white">로그인</v-toolbar-title>
+          </v-toolbar>
+
+          <div class="pa-3">
+            <v-text-field label="Email address" v-model="email"></v-text-field>
+            <v-text-field
+              label="password"
+              type="password"
+              v-model="password"
+            ></v-text-field>
+          </div>
+          <v-btn large block depressed color="primary" @click="login"
+            >로그인</v-btn
+          >
+        </v-card>
       </v-col>
-     </v-row>
-   </v-container>
+    </v-row>
+  </v-container>
 </template>
 <script>
-export default {
-   name: '',
-   components: {
-     
-   },
-   mixins: [],
-   props: {
-     
-   },
-   data() {
-     return {
-       email: null,
-       password: null,
-       allUsers: [
-        {id: 1, name: 'hyebin', email: 'khv@naver.com', password: '1234'},
-        {id: 2, name: 'logo', email: 'khv@naver.com', password: '1234'}
-       ],
-       isError: false,
-       loginSuccess: false
-     }
-   },
-   computed: {
-     
-   },
-   watch: {
-     
-   },
-   mounted() {
-     
-   },
-   methods: {
-     login() {
-      // 전체 유저에서 해당 이메일로 유저를 찾는다.
-      let selectedUser = null
-      this.allUsers.forEach(user=>{
-        if(user.email === this.email) selectedUser = user
-      })
-      if(selectedUser === null) this.isError= true
-      else {
-        selectedUser.password !== this.password?  this.isError= true: this.loginSuccess = true
-      }
-      // 그 유저의 비밀번호와 입력된 비밀번호를 비교한다.
-     }
-   }
-};
+  export default {
+    name: "",
+    components: {},
+    mixins: [],
+    props: {},
+    data() {
+      return {
+        email: null,
+        password: null,
+        allUsers: [
+          { id: 1, name: "hyebin", email: "khv@naver.com", password: "1234" },
+          { id: 2, name: "logo", email: "khv@naver.com", password: "1234" },
+        ],
+        isError: false,
+        loginSuccess: false,
+      };
+    },
+    computed: {},
+    watch: {},
+    mounted() {},
+    methods: {
+      login() {
+        // 전체 유저에서 해당 이메일로 유저를 찾는다.
+        let selectedUser = null;
+        this.allUsers.forEach((user) => {
+          if (user.email === this.email) selectedUser = user;
+        });
+        if (selectedUser === null) this.isError = true;
+        else {
+          selectedUser.password !== this.password
+            ? (this.isError = true)
+            : (this.loginSuccess = true);
+        }
+        // 그 유저의 비밀번호와 입력된 비밀번호를 비교한다.
+      },
+    },
+  };
 </script>
-<style lang='' scoped>
-</style>
+<style lang="" scoped></style>
